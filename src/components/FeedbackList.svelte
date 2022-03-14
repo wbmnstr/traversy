34634737a8716672db48701a12bb8efc9eae132a
wbmnstr <fatihsaridag@gmail.com>
2022-03-14 07:52:45 +0300

    first commit

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {FeedbackStore} from '../stores';
+    import { fade, scale, blur, slide } from 'svelte/transition';
+    import FeedbackItem from './FeedbackItem.svelte';
+    let feedback=[];
+
+    FeedbackStore.subscribe((data)=>feedback=data);
+</script>
+
+{#each feedback as fb (fb.id)}
+
+<div in:slide out:fade>
+    <FeedbackItem item={fb} on:delete-feedback />
+</div>
+
+{/each}
